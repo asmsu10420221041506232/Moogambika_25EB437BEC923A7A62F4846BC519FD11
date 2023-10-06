639281b7424f6df5f013bec685e5acb89b5fd575
asmsu10420221041506232 <144543475+asmsu10420221041506232@users.noreply.github.com>
2023-10-06 18:14:20 +0530

    Add files via upload

--- main (4).py
+++ main (4).py
@@ -1,10 +1,13 @@
-def fact_rec(n):
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return n * fact_rec(n - 1)
-
-
-number = int(input("Enter a value:"))
-res = fact_rec(number)
-print("The factorial of {} is {}.".format(number, res))
+def linear_search_product(product_list, target_product):
+    indices = []
+    for i, product in enumerate(product_list):
+        if product == target_product:
+            indices.append(i)
+    return indices
+products = ["apple", "banana", "apple", "orange", "apple"]
+target = "apple"
+result = linear_search_product(products, target)
+if result:
+    print(f"The product '{target}' was found at indices: {result}")
+else:
+    print(f"The product '{target}' was not found in the list.")
